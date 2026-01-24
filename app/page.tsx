04594e393ce72b1c2e32a256627f85169ec7f591
vinoth kumar <87934181+vinothwino@users.noreply.github.com>
2026-01-24 17:17:46 +0530

    feat: added skill section (#5)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AboutSection from "@/components/aboutSection/AboutSection";
 import { HomeBanner } from "@/components/HomeBanner";
+import { SkillSection } from "@/components/skillsSection/SkillSection";
 
 export default function Home() {
   return (
     <>
       <HomeBanner />
       <AboutSection />
+      <SkillSection />
     </>
   );
 }
